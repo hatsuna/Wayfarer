1b15432a429880b0ab575c2f3dd172fd73f710a7
[Hatsuna] <ixnoblesseobligexi@gmail.com>
2016-01-07 19:13:13 -0500

    Prototype player model, rig, animation, animation controller, camera controls

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public Animator myAnimator1;
+	public Transform myCamera;
+
+	float moveSpeed = 5f;
+	Rigidbody rbody1;
+	Vector3 inputVector;
+	bool isWalking = false;
+	bool isAttacking = false;
+
+	float storedRotation = 0;
+
+	// Use this for initialization
+	void Start () {
+		rbody1 = GetComponent<Rigidbody>();
+	}
+
+	void Update(){
+		inputVector = new Vector3(Input.GetAxis("Horizontal"),  0 /* jump*/, Input.GetAxis ("Vertical"));
+		float mouseX = Input.GetAxis("Mouse X");
+		storedRotation += mouseX;
+
+		if( Input.GetAxis("Horizontal") == 0 && Input.GetAxis ("Vertical") == 0){
+			isWalking = false;
+		} else{
+			myCamera.transform.RotateAround(transform.position, Vector3.up, -storedRotation);
+			transform.Rotate(0f, storedRotation, 0f);
+			isWalking = true;
+			storedRotation = 0.0f;
+		}
+
+		if(Input.GetMouseButtonDown(0)){
+			myAnimator1.SetTrigger("isAttacking");
+			isWalking = false;
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float yVelocity = rbody1.velocity.y;
+		rbody1.velocity = transform.TransformDirection(inputVector) * moveSpeed;
+		rbody1.velocity += new Vector3( 0f, yVelocity, 0f);
+
+		myAnimator1.SetBool("isWalking", isWalking);
+
+
+
+	}
+}
