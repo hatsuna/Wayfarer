7c5af161c77de5decd48b7cf4c07dd1b4b56c2c4
[Hatsuna] <ixnoblesseobligexi@gmail.com>
2016-01-10 17:23:47 -0500

    prototype hit detection, attachments

---
+++ Assets/Scripts/HitDetection.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitDetection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if(collision.collider.gameObject.tag == "Player"){
+			Debug.Log("Dude, don't touvh me there");
+		}
+
+		if(collision.collider.gameObject.tag == "Weapon"){
+			//gameObject.GetComponent<Material>().color = Color.red;
+			Debug.Log("hit!");
+		}
+	}
+}
