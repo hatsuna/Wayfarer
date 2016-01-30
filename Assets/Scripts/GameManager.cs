b792de0745aec0879c39c8db5e96063ff298af16
Matthew Pon <ixnoblesseobligexi@gmail.com>
2016-01-30 17:11:07 +0900

    Prototype sound + cut up animations + start screen + other stuff

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	//public Text textUI;
+	public Skybox skybox;
+
+	public Camera bookCamera;
+	public Camera battleCamera;
+
+	public Canvas startUI;
+	public Canvas bookUI;
+	public Canvas battleUI;
+
+	string GameState = "Start";
+	/*
+	 * Start
+	 * Book
+	 * Battle
+	 */
+
+	// Use this for initialization
+	void Start () {
+		bookCamera.gameObject.SetActive(true);
+		battleCamera.gameObject.SetActive(false);
+
+		startUI.gameObject.SetActive(true);
+		bookUI.gameObject.SetActive(false);
+		battleUI.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	switch(GameState){
+		case "Start":
+			Text startText = startUI.GetComponentInChildren<Text>();
+				startText.text = "Wayfarer\n" +
+				"a game by matt\n\n" +
+					"press [Space] to continue";
+
+
+			if(Input.GetKeyDown (KeyCode.Space)){
+				startText.text = "";
+				GameState = "Battle";
+			}
+			break;
+		case "Book":
+			startUI.gameObject.SetActive(false);
+			bookUI.gameObject.SetActive(true);
+			battleUI.gameObject.SetActive(false);
+
+			break;
+		case "Battle":
+			bookCamera.gameObject.SetActive(false);
+			battleCamera.gameObject.SetActive(true);
+
+			startUI.gameObject.SetActive(false);
+			bookUI.gameObject.SetActive(false);
+			battleUI.gameObject.SetActive(true);
+
+			break;
+		default:
+			break;
+		}
+
+				//restart Level
+		if(Input.GetKeyDown(KeyCode.R)){
+			Application.LoadLevel(0);
+		}
+	}
+}
--- Assets/Scripts/HitDetection.cs
+++ Assets/Scripts/HitDetection.cs
@@ -4,11 +4,15 @@ using System.Collections;
 public class HitDetection : MonoBehaviour {
 
 	//This goes on weapons
-
+	Animator charAnim; //set in start
+	AudioSource audio;
+	public AudioClip sword_collide;
+	public AudioClip hurt;
 
 	// Use this for initialization
 	void Start () {
-
+		charAnim = GetComponentInParent<Animator>();
+		audio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -17,13 +21,21 @@ public class HitDetection : MonoBehaviour {
 
 	void OnCollisionEnter(Collision collision){
 		Debug.Log(gameObject.name + " collided with " + collision.collider.gameObject.name);
-		if(collision.collider.gameObject.tag == "Player"){
-			Debug.Log("hit!");
-		}
 
 		if(collision.collider.gameObject.tag == "Weapon"){
 			//gameObject.GetComponent<Material>().color = Color.red;
 			Debug.Log("parried!");
+			charAnim.SetTrigger("isParried");
+			audio.clip = sword_collide; 
+			audio.volume = 0.1f;
+			audio.Play();
+		} else if(collision.collider.gameObject.tag == "Player"){
+			Debug.Log("hit!");
+			audio.clip = hurt;
+			audio.volume = 1.0f;
+			audio.Play();
 		}
+	
+
 	}
 }
