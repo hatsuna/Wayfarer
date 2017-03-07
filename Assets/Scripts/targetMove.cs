09dec6fee45ba606063cadf329c0dfd8a1b1f153
Matthew Pon <ixnoblesseobligexi@gmail.com>
2017-03-07 16:37:21 -0500

    Navmesh & prototype AI update

--- Assets/Scripts/targetMove.cs
+++ Assets/Scripts/targetMove.cs
@@ -1,31 +1,91 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 using System.Collections;
 
 public class targetMove : MonoBehaviour {
 
+	Vector3 spawn;
+	Color color;
+	float timeStarted = 0;
+	float timeReset = 1.0f; // time until color resets
+	float impulseOfHit = 25.0f;
+	ParticleSystem particles;
+	ParticleSystem.MainModule main;
+
+	bool hit = false;
 	Rigidbody rbody;
 	float movespeed = 3.0f;
-	public Transform player;
+	Transform player;
+	NavMeshAgent agent;
+	GameObject bulletLauncher;
 
 	// Use this for initialization
 	void Start () {
+		player = Camera.main.transform;
+		agent = GetComponent<NavMeshAgent>();
 		rbody = GetComponent<Rigidbody>();
+		bulletLauncher = GetComponentInChildren<bulletManager>().gameObject;
+		agent.stoppingDistance = 20.0f;
+
+		particles = GetComponent<ParticleSystem>();
+		main = particles.main;
+	}
+
+	void Awake(){
+		spawn = transform.position;
+	}
+
+	void Update(){
+		timeStarted += Time.deltaTime;
+		if (timeStarted > timeReset){
+			GetComponent<MeshRenderer>().material.color = Color.white;
+			particles.Stop();
+		}
+
+		if(hit){
+			StartCoroutine("BleedOutAndDie");
+		}
 	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		//rbody.velocity = transform.right * movespeed + Physics.gravity;
-		/*Ray moveRay = new Ray(transform.position, transform.right);
-		if(Physics.SphereCast(moveRay, 0.5f, 3f)){
-			float rand = Random.value;
-			if(rand >= 0.5f){
-				transform.Rotate(0, 90, 0);
-			} else {
-				transform.Rotate(0, 90, 0);
-			}
-		}*/
-
+		agent.destination = player.position;
 		Vector3 directionToPlayer = player.position - transform.position;
-		transform.forward = directionToPlayer;
+		bulletLauncher.transform.forward = directionToPlayer;
+	}
+
+	void OnCollisionEnter(Collision collision){
+		//Debug.Log(gameObject.name + " collided with " + collision.collider.gameObject.name);
+
+		Material newMaterial = new Material(Shader.Find("Standard"));
+
+		int collidedLayer = collision.gameObject.layer;
+		if(collidedLayer == 10 || collidedLayer == 8){
+			if(collision.impulse.magnitude >= impulseOfHit){
+				newMaterial.SetColor("_Color", Color.red);
+				hit = true;
+				main.startColor = Color.red;
+			} else{
+				newMaterial.SetColor("_Color", Color.grey);
+				hit = false;
+				main.startColor = Color.white;
+			}
+			if (particles.isPlaying == false){
+				particles.Play();
+			}
+		}
+		GetComponent<MeshRenderer>().material = newMaterial;
+		timeStarted = 0;
+	}
+
+	IEnumerator BleedOutAndDie(){
+		main.loop = true;
+		if(!particles.isPlaying){
+			particles.Play();
+		}
+		yield return new WaitForSeconds(5);
+		gameObject.SetActive(false);
+		transform.position = spawn;
+		gameObject.SetActive(true);
 	}
 }
